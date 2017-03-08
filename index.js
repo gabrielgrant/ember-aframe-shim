50942a38a93057583c12a4f2ce96abadce70c06c
Kelly Selden <kellyselden@gmail.com>
2017-03-07 20:36:28 -0800

    browserify test

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 const path = require('path');
 const Funnel = require('broccoli-funnel');
+var watchify = require('broccoli-watchify');
+const mergeTrees = require('broccoli-merge-trees');
 
 const packageName = 'aframe';
 
@@ -13,20 +15,52 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     let resolvedPath = require.resolve(packageName);
-    this.dirname = path.dirname(resolvedPath);
-    this.basename = path.basename(resolvedPath);
+    this.dirname = path.join(path.dirname(resolvedPath), '..');
 
-    this.import(`vendor/${packageName}/${this.basename}`, {
-      using: [
-        { transformation: 'amd', as: packageName }
-      ]
-    });
+    this.import(`vendor/${packageName}/aframe.js`
+      , {
+        using: [
+          { transformation: 'amd', as: packageName }
+        ]
+      }
+    );
+
+    // this.import('vendor/Tween.js'
+    //   , {
+    //     using: [
+    //       { transformation: 'amd', as: 'tween.js' }
+    //     ]
+    //   }
+    // );
   },
 
   treeForVendor() {
-    return new Funnel(this.dirname, {
-      files: [this.basename],
+    let tree = new Funnel(this.treeGenerator(`${this.dirname}`), {
+      // files: ['index.js'],
       destDir: packageName
     });
+
+    let trees = [];
+
+    trees.push(watchify(tree, {
+      browserify: {
+        entries: [`./${packageName}/src/index.js`],
+        debug: true,
+        plugin: ['browserify-derequire'],
+        standalone: 'aframe'
+      },
+      outputFile: `${packageName}/aframe.js`,
+      cache: true,
+      // init: function (b) {
+      //   b.transform('reactify', {'es6': true});
+      //   b.external('$');
+      // }
+    }));
+
+    // trees.push(new Funnel(path.dirname(require.resolve('tween.js')), {
+    //   files: ['Tween.js']
+    // }));
+
+    return mergeTrees(trees);
   }
 };
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import AFrame from 'aframe';
+import constants from 'aframe/constants';
 
 export default Ember.Controller.extend({
   aframe: AFrame
